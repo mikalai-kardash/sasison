478ec0848ec74305e0558270e4b59d1a8bff69b1
Mikalai Kardash <mikalai.kardash@gmail.com>
2016-07-21 15:36:09 -0700

    Initial checkin.

---
+++ src/sasison/ISassCompiler.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace sasison
+{
+    // todo: compilation on-the-fly
+    // todo: variable overrides, additional chunks, etc.
+    public interface ISassCompiler
+    {
+        string Compile(string input);
+    }
+}
---
+++ src/sasison/SassCompiler.cs
@@ -0,0 +1,10 @@
+﻿namespace sasison
+{
+    public class SassCompiler : ISassCompiler
+    {
+        public string Compile(string input)
+        {
+            return string.Empty;
+        }
+    }
+}
